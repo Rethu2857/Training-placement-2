c1e501e5f84c9de2e0b977496d247204e94d31a9
Rethu2857 <rethanyasri9@gmail.com>
2025-12-22 20:46:10 +0530

    Create symmetric tree.java

---
+++ 20.12.2025/symmetric tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        mirror(root.left);
+        return isSameTree(root.left,root.right);
+    }
+    void mirror(TreeNode root) {
+        if(root== null) return;
+        TreeNode temp = root.left;
+        root.left =  root.right;
+        root.right = temp;
+        mirror(root.left);
+        mirror(root.right);
+    }
+
+    boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null || q== null) return false;
+        if(p.val!=q.val) return false;
+        if(!isSameTree(p.left,q.left) || !isSameTree(p.right,q.right)) return false;
+        return true;
+    }
+}\
