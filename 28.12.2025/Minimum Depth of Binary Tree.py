5d36b225ef1956b03056804220f3741e93118edd
Rethu2857 <rethanyasri9@gmail.com>
2025-12-28 23:06:56 +0530

    Create Minimum Depth of Binary Tree.py

---
+++ 28.12.2025/Minimum Depth of Binary Tree.py
@@ -0,0 +1,8 @@
+def min_depth(root):
+    if not root:
+        return 0
+    if not root.left:
+        return 1 + min_depth(root.right)
+    if not root.right:
+        return 1 + min_depth(root.left)
+    return 1 + min(min_depth(root.left), min_depth(root.right))
