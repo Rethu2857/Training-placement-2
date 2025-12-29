0ca8ec80f4a6dfb837c7752d41e754dc33b0cb6d
Rethu2857 <rethanyasri9@gmail.com>
2025-12-29 16:00:15 +0530

    Create merge_k_sorted_lists.py

---
+++ 29.12.2025/merge_k_sorted_lists.py
@@ -0,0 +1,18 @@
+import heapq
+
+def mergeKLists(lists):
+    heap = []
+    for i, l in enumerate(lists):
+        if l:
+            heapq.heappush(heap, (l.val, i, l))
+
+    dummy = curr = ListNode(0)
+
+    while heap:
+        val, i, node = heapq.heappop(heap)
+        curr.next = node
+        curr = curr.next
+        if node.next:
+            heapq.heappush(heap, (node.next.val, i, node.next))
+
+    return dummy.next
