7b0e6fe9148c7050a432edc6d8f2197005b87f2b
Rethu2857 <rethanyasri9@gmail.com>
2025-12-29 16:01:12 +0530

    Create sliding_window_maximum.py

---
+++ 29.12.2025/sliding_window_maximum.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def maxSlidingWindow(nums, k):
+    dq = deque()
+    res = []
+
+    for i, n in enumerate(nums):
+        while dq and dq[0] <= i - k:
+            dq.popleft()
+        while dq and nums[dq[-1]] < n:
+            dq.pop()
+        dq.append(i)
+        if i >= k - 1:
+            res.append(nums[dq[0]])
+    return res
