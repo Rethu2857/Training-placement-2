20d6830287be7e3aacbf84369a091b9d8fe0174a
Rethu2857 <rethanyasri9@gmail.com>
2025-12-29 15:58:18 +0530

    Create longest_valid_parentheses.py

---
+++ 29.12.2025/longest_valid_parentheses.py
@@ -0,0 +1,14 @@
+def longestValidParentheses(s):
+    stack = [-1]
+    res = 0
+
+    for i, ch in enumerate(s):
+        if ch == '(':
+            stack.append(i)
+        else:
+            stack.pop()
+            if not stack:
+                stack.append(i)
+            else:
+                res = max(res, i - stack[-1])
+    return res
