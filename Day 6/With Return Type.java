bb6a599ad5bc6eeb90b924b9f69d0b9984b09836
Rethu2857 <rethanyasri9@gmail.com>
2026-03-16 16:33:06 +0530

    Add multiply method with return type in Main class

---
+++ Day 6/With Return Type.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static void main(String[] args) {
+        int result = multiply(4, 5);
+        System.out.println("Result = " + result);
+    }
+}
